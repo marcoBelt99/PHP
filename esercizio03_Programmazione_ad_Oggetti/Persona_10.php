64bc324e54ef1952597ee8bc6a2cb60df1bf0c0c
marcoBelt99 <marco01.beltrame@edu.unife.it>
2023-04-19 17:15:09 +0200

    traits

---
+++ esercizio03_Programmazione_ad_Oggetti/Persona_10.php
@@ -0,0 +1,39 @@
+<?php
+/** PHP supporta solo l'ereditarieta' singola. Nonostante cio', tale linguaggio cerca di superare questa limitazione consentendo il riuso
+ *  di metodi presenti in differenti classi indipendentemente dal vincolo gerarchico della singola derivazione. Tale meccanismo è noto come
+ *  traits. In somma sintesi si tratta di un modo per organizzare pezzi di codice che possono essere poi richiamati, ma non per un contesto di
+ *  ereditarieta', ma come se fossero semplicemente inclusi.
+ */
+
+ // TRAIT
+trait Saluti // in buona sostanza questo trait 'Saluti' e' una sorta di classe
+{
+    public function italiano()
+    {
+        return "Salve, mondo!</br>";
+    }
+
+    public function inglese()
+    {
+        return "Hello, world!</br>";
+    }
+
+    public function francese()
+    {
+        return "Bonjour, le monde!</br>";
+    }
+
+}
+
+    
+    class Benvenuto {
+        use Saluti;    // Posso richiamare tale pezzo di codice in una mia qualsiasi classe usando la parola chiave use
+    }
+
+
+    $b = new Benvenuto();
+    echo $b->italiano();
+    echo "<br>";
+    echo $b->inglese();
+
+?>
